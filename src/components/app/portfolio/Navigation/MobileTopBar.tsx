de0f9ad449a8c980fe421b472e5a1dfa1d14d1f2
NiainaFranco <niainafrancoroberto@gmail.com>
2026-05-05 15:21:36 +0300

    feat: add mobile sidebar with actual links

--- src/components/app/portfolio/Navigation/MobileTopBar.tsx
+++ src/components/app/portfolio/Navigation/MobileTopBar.tsx
@@ -3,9 +3,12 @@
 import { HamburgerIcon } from "@/components/DynamiqueIcons/HamburgerIcon";
 import { SideBar } from "./SideBar";
 import { useState } from "react";
+import { useTranslations } from "next-intl";
+import { CrossIcon } from "@/components/DynamiqueIcons/CrossIcon";
 
 export function MobileTopBar() {
   const [showSidebar, setShowSidebar] = useState(false);
+  const tnavigation = useTranslations("Navigation")
   return (
     <nav className="w-full top-0 block md:hidden lg:hidden left-0 relative z-40 sticky">
       <div className="p-3 bg-primary-500">
@@ -21,7 +24,43 @@ export function MobileTopBar() {
           </button>
         </div>
       </div>
-      <SideBar opened={showSidebar}>something</SideBar>
+      <SideBar setOpened={setShowSidebar} opened={showSidebar}>
+        <div className="h-full min-w-[200px] bg-primary-500">
+            <button onClick={()=>setShowSidebar(false)} className="w-[60px] h-[60px]">
+              <CrossIcon className="fill-white" />
+            </button>
+          <div
+            className={`
+            flex-col
+            [&>a]:rounded-xl
+            [&>a]:hover:bg-primary-400
+            [&>a]:duration-[500ms]
+            [&>a]:p-2 
+            text-white
+            text-xl
+            `}
+          >
+            <a className="w-[fit-content] relative block" href="#hero">
+              {tnavigation("home")}
+            </a>
+            <a className="w-[fit-content] relative block" href="#services">
+              {tnavigation("services")}
+            </a>
+            <a className="w-[fit-content] relative block" href="#about-me">
+              {tnavigation("aboutMe")}
+            </a>
+            <a className="w-[fit-content] relative block" href="#projects">
+              {tnavigation("projects")}
+            </a>
+            <a className="w-[fit-content] relative block" href="#pricing">
+              {tnavigation("pricing")}
+            </a>
+            <a className="w-[fit-content] relative block" href="#contacts">
+              {tnavigation("contacts")}
+            </a>
+          </div>
+        </div>
+      </SideBar>
     </nav>
   );
 }
--- src/components/app/portfolio/Navigation/SideBar.tsx
+++ src/components/app/portfolio/Navigation/SideBar.tsx
@@ -1,27 +1,34 @@
 "use client"
 
 import { useDocumentRootRef } from "@/hooks/useOverlayRef"
-import { ReactNode, useEffect } from "react"
+import { Dispatch, ReactNode, SetStateAction } from "react"
 import { createPortal } from "react-dom"
 
 type SideBarPropsType = {
     children: ReactNode
     opened?: boolean
+    setOpened: Dispatch<SetStateAction<boolean>>
 }
 
 export function SideBar(props: SideBarPropsType) {
-  const { children, opened } = props;
+  const { children, opened, setOpened} = props;
   const documentRootRef = useDocumentRootRef();
   return (
     documentRootRef && opened &&
-    createPortal(<Wrapper>{children}</Wrapper>, documentRootRef)
+    createPortal(<Wrapper setOpened={setOpened}>{children}</Wrapper>, documentRootRef)
   );
-}
+};
 
-function Wrapper(props: {children: ReactNode}){
-    return (
-        <div className="w-screen fixed top-0 left-0 overflow-hidden bg-[#00000070] h-screen ">
-            {props.children}
-        </div>
-    )
+function Wrapper(props: {
+  children: ReactNode;
+  setOpened: Dispatch<SetStateAction<boolean>>;
+}) {
+  return (
+    <div className="w-screen flex fixed top-0 left-0 overflow-hidden z-90 h-screen">
+      <div className="w-[fit-content] h-full">
+        {props.children}
+      </div>
+      <div onClick={()=>props.setOpened(false)} className="flex-1 bg-[#00000070]"></div>
+    </div>
+  );
 }
